d2ce702f38bb331562afae2c04bfefc6ef6b6f0c
Kamil <kamil.slosarczyk@yahoo.com>
2020-03-30 14:25:33 +0100

    Few changes to CSS file

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('./serviceWorker.js', { scope: './' })
         .then(function(registration) {
-            console.log("Service Worker Registered");
+            console.log("Service Worker Registered. Scope is:"+registration.scope);
         })
         .catch(function(err) {
             console.log("Service Worker Failed to Register", err);
