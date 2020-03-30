7e9fe3cb9ff2d78b36a04dcbca5751174b297006
Kamil <kamil.slosarczyk@yahoo.com>
2020-03-30 13:43:14 +0100

    Few changes to CSS file

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,10 +1,10 @@
 const cacheName = 'v1';
 const cacheFiles = [
     './index.html',
+    './',
     'img/icon-72x72.png',
     'img/icon-96x96.png',
     'img/icon-128x128.png',
-    'img/icon-144x144.png',
     'img/icon-152x152.png',
     'img/icon-192x192.png',
     'img/icon-384x384.png',
